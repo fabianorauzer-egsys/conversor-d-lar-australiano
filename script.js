d03d17ef582ef9907e0d82d251306b0ad6dbd70a
f <fabiano.rauzer@egsys.com.br>
2023-03-22 09:47:16 -0300

    Trazendo o valor do dólar da API e caso ela esteja dando problema, setando a cotação para 3.51

--- script.js
+++ script.js
@@ -1,65 +1,59 @@
+import { getDolarAustraliano } from './api.js';
+
 const CACHE_KEY = 'conversor_cache';
 
 function getCache() {
-    let cache = localStorage.getItem(CACHE_KEY);
-    if (!cache) {
-        cache = '[]';
-    }
-    return JSON.parse(cache);
+  let cache = localStorage.getItem(CACHE_KEY);
+  if (!cache) {
+    cache = '[]';
+  }
+  return JSON.parse(cache);
 }
 
 function setCache(valores) {
-    localStorage.setItem(CACHE_KEY, JSON.stringify(valores));
+  localStorage.setItem(CACHE_KEY, JSON.stringify(valores));
 }
 
-async function getDolarAustraliano() {
-    const response = await fetch(`http://data.fixer.io/api/latest?access_key=HAJy9BBZ0avu5gigNM9O1hNXAPTJZYlX&symbols=AUD`);
-    const data = await response.json();
-    const cotacao = data.rates.AUD;
-    return cotacao;
-}
-
-async function converterDolaresParaReais(valor) {
-    const cotacao = await getDolarAustraliano();
-    return valor * cotacao;
+function converterDolaresParaReais(valor) {
+  return valor * (isNaN(valor) ? 3.51 : valor);
 }
 
 function adicionarLinha(reais, dolares, descricao, temAcomodacao) {
-    const tbody = document.querySelector('#tabela tbody');
-    const linha = document.createElement('tr');
+  const tbody = document.querySelector('#tabela tbody');
+  const linha = document.createElement('tr');
 
-    const colunaReais = document.createElement('td');
-    colunaReais.textContent = reais.toFixed(2);
+  const colunaReais = document.createElement('td');
+  colunaReais.textContent = isNaN(reais) ? '3.51' : reais.toFixed(2);
 
-    const colunaDolares = document.createElement('td');
-    colunaDolares.textContent = dolares.toFixed(2);
+  const colunaDolares = document.createElement('td');
+  colunaDolares.textContent = dolares.toFixed(2);
 
-    const colunaDescricao = document.createElement('td');
-    colunaDescricao.textContent = descricao;
+  const colunaDescricao = document.createElement('td');
+  colunaDescricao.textContent = descricao;
 
-    const colunaAcomodacao = document.createElement('td');
-    colunaAcomodacao.textContent = temAcomodacao ? 'Sim' : 'Não';
+  const colunaAcomodacao = document.createElement('td');
+  colunaAcomodacao.textContent = temAcomodacao ? 'Sim' : 'Não';
 
-    const colunaAcoes = document.createElement('td');
-    const botaoExcluir = document.createElement('button');
-    botaoExcluir.textContent = 'Excluir';
-    botaoExcluir.addEventListener('click', () => {
-        linha.remove();
-        const cache = getCache();
-        const index = cache.findIndex(([r, d, desc, acom]) => r === reais && d === dolares && desc === descricao && acom === temAcomodacao);
-        if (index !== -1) {
-            cache.splice(index, 1);
-            setCache(cache);
-        }
-    });
-    colunaAcoes.appendChild(botaoExcluir);
+  const colunaAcoes = document.createElement('td');
+  const botaoExcluir = document.createElement('button');
+  botaoExcluir.textContent = 'Excluir';
+  botaoExcluir.addEventListener('click', () => {
+    linha.remove();
+    const cache = getCache();
+    const index = cache.findIndex(([r, d, desc, acom]) => r === reais && d === dolares && desc === descricao && acom === temAcomodacao);
+    if (index !== -1) {
+      cache.splice(index, 1);
+      setCache(cache);
+    }
+  });
+  colunaAcoes.appendChild(botaoExcluir);
 
-    linha.appendChild(colunaReais);
-    linha.appendChild(colunaDolares);
-    linha.appendChild(colunaDescricao);
-    linha.appendChild(colunaAcomodacao);
-    linha.appendChild(colunaAcoes);
-    tbody.appendChild(linha);
+  linha.appendChild(colunaReais);
+  linha.appendChild(colunaDolares);
+  linha.appendChild(colunaDescricao);
+  linha.appendChild(colunaAcomodacao);
+  linha.appendChild(colunaAcoes);
+  tbody.appendChild(linha);
 }
 
 function atualizarTabela() {
@@ -73,35 +67,36 @@ function atualizarTabela() {
     }
 }
 
-function onSubmitDolar(event) {
-    event.preventDefault();
+async function onSubmitDolar(event) {
+  event.preventDefault();
 
-    const valorInput = document.querySelector('#valor-dolar');
-    const valor = parseFloat(valorInput.value);
+  const valorInput = document.querySelector('#valor-dolar');
+  const valor = parseFloat(valorInput.value);
 
-    const descricaoInput = document.querySelector('#descricao-dolar');
-    const descricao = descricaoInput.value;
+  const descricaoInput = document.querySelector('#descricao-dolar');
+  const descricao = descricaoInput.value;
 
-    const acomodacaoInput = document.querySelector('#acomodacao-dolar');
-    const temAcomodacao = acomodacaoInput.checked;
+  const acomodacaoInput = document.querySelector('#acomodacao-dolar');
+  const temAcomodacao = acomodacaoInput.checked;
 
-    if (!isNaN(valor)) {
-        const reais = converterDolaresParaReais(valor);
-        adicionarLinha(reais, valor, descricao, temAcomodacao);
+  if (!isNaN(valor)) {
+    const cotacao = await getDolarAustraliano();
+    const reais = valor * cotacao;
+    adicionarLinha(reais, valor, descricao, temAcomodacao);
 
-        const cache = getCache();
-        cache.push([reais, valor, descricao, temAcomodacao]);
-        setCache(cache);
+    const cache = getCache();
+    cache.push([reais, valor, descricao, temAcomodacao]);
+    setCache(cache);
 
-        valorInput.value = '';
-        descricaoInput.value = '';
-        acomodacaoInput.checked = false;
-    }
+    valorInput.value = '';
+    descricaoInput.value = '';
+    acomodacaoInput.checked = false;
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    atualizarTabela();
+  atualizarTabela();
 
-    const formDolar = document.querySelector('#form-dolar');
-    formDolar.addEventListener('submit', onSubmitDolar);
+  const formDolar = document.querySelector('#form-dolar');
+  formDolar.addEventListener('submit', onSubmitDolar);
 });
