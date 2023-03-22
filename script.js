4839bdd33d9a1f9f57fb1de6c247694b4ba94c10
f <fabiano.rauzer@egsys.com.br>
2023-03-22 09:52:25 -0300

    Adicionada lógica para excluir do cache quando o botão excluir for clicado

--- script.js
+++ script.js
@@ -15,42 +15,43 @@ function setCache(valores) {
 }
 
 function adicionarLinha(reais, dolares, descricao, temAcomodacao) {
-  const tbody = document.querySelector('#tabela tbody');
-  const linha = document.createElement('tr');
-
-  const colunaReais = document.createElement('td');
-  colunaReais.textContent = isNaN(reais) ? '3.51' : reais.toFixed(2);
-
-  const colunaDolares = document.createElement('td');
-  colunaDolares.textContent = dolares.toFixed(2);
-
-  const colunaDescricao = document.createElement('td');
-  colunaDescricao.textContent = descricao;
-
-  const colunaAcomodacao = document.createElement('td');
-  colunaAcomodacao.textContent = temAcomodacao ? 'Sim' : 'Não';
-
-  const colunaAcoes = document.createElement('td');
-  const botaoExcluir = document.createElement('button');
-  botaoExcluir.textContent = 'Excluir';
-  botaoExcluir.addEventListener('click', () => {
-    linha.remove();
-    const cache = getCache();
-    const index = cache.findIndex(([r, d, desc, acom]) => r === reais && d === dolares && desc === descricao && acom === temAcomodacao);
-    if (index !== -1) {
-      cache.splice(index, 1);
-      setCache(cache);
-    }
-  });
-  colunaAcoes.appendChild(botaoExcluir);
-
-  linha.appendChild(colunaReais);
-  linha.appendChild(colunaDolares);
-  linha.appendChild(colunaDescricao);
-  linha.appendChild(colunaAcomodacao);
-  linha.appendChild(colunaAcoes);
-  tbody.appendChild(linha);
-}
+    const tbody = document.querySelector('#tabela tbody');
+    const linha = document.createElement('tr');
+  
+    const colunaReais = document.createElement('td');
+    colunaReais.textContent = isNaN(reais) ? '3.51' : reais.toFixed(2);
+  
+    const colunaDolares = document.createElement('td');
+    colunaDolares.textContent = dolares.toFixed(2);
+  
+    const colunaDescricao = document.createElement('td');
+    colunaDescricao.textContent = descricao;
+  
+    const colunaAcomodacao = document.createElement('td');
+    colunaAcomodacao.textContent = temAcomodacao ? 'Sim' : 'Não';
+  
+    const colunaAcoes = document.createElement('td');
+    const botaoExcluir = document.createElement('button');
+    botaoExcluir.textContent = 'Excluir';
+    botaoExcluir.addEventListener('click', () => {
+      linha.remove();
+  
+      const cache = getCache();
+      const index = cache.findIndex(([r, d, desc, acom]) => r === reais && d === dolares && desc === descricao && acom === temAcomodacao);
+      if (index !== -1) {
+        cache.splice(index, 1);
+        setCache(cache);
+      }
+    });
+    colunaAcoes.appendChild(botaoExcluir);
+  
+    linha.appendChild(colunaReais);
+    linha.appendChild(colunaDolares);
+    linha.appendChild(colunaDescricao);
+    linha.appendChild(colunaAcomodacao);
+    linha.appendChild(colunaAcoes);
+    tbody.appendChild(linha);
+  }
 
 function atualizarTabela() {
     const cache = getCache();
